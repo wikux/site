d9d33b168383757c9f18e4b66c87bc69e00d8d05
wlft <100052562+wlft@users.noreply.github.com>
2026-07-19 03:56:07 +1000

    more inpsirational sites

--- src/lib/inspiration.ts
+++ src/lib/inspiration.ts
@@ -11,13 +11,25 @@ export const inspiration_wikis: InspirationWiki[] = [
     name: "Star Citizen Wiki",
     url: "https://starcitizen.tools/",
     image: "/insp/starcitizentools.png",
-    tags: ["Citizen", "Dark theme", "Gaming"],
+    tags: ["Citizen", "Gaming"],
+  },
+  {
+    name: "Subnautica Wiki",
+    url: "https://wiki.subnautica.com/",
+    image: "/insp/subnauticawiki.png",
+    tags: ["Vector", "Gaming", "Independent"],
   },
   {
     name: "Item Asylum Wiki",
     url: "https://itemasylum.wiki/",
     image: "/insp/item-asylum.png",
     tags: ["Citizen", "Roblox", "Miraheze"],
+  },
+  {
+    name: "Runescape Wiki",
+    url: "https://runescape.wiki/",
+    image: "/insp/runescapewiki.png",
+    tags: ["Vector", "Gaming", "Independent"],
   },
   {
     name: "untitled tag game wiki",
@@ -29,6 +41,24 @@ export const inspiration_wikis: InspirationWiki[] = [
     name: "Minecraft Wiki",
     url: "https://minecraft.wiki/",
     image: "/insp/minecraft.png",
+    tags: ["Vector", "Gaming", "Independent"],
+  },
+  {
+    name: "Hytale Wiki",
+    url: "https://hytalewiki.org/",
+    image: "/insp/hytalewiki.png",
+    tags: ["Vector", "Gaming", "Independent"],
+  },
+  {
+    name: "Nookipedia",
+    url: "https://nookipedia.com/",
+    image: "/insp/nookipedia.png",
+    tags: ["Vector", "Gaming", "Independent"],
+  },
+  {
+    name: "Valorant Wiki",
+    url: "https://wiki.playvalorant.com/",
+    image: "/insp/valorantwiki.png",
     tags: ["Vector", "Gaming", "Independent"],
   },
   {
